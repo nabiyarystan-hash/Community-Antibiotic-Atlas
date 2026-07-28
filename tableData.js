c1ab70c51e3f2215b594bc56c42a40d058287332
nabiyarystan-hash <nabiy.arystan@nu.edu.kz>
2026-07-28 13:54:02 +0500

    Create tableData.js

---
+++ tableData.js
@@ -0,0 +1,20 @@
+const tableData = [
+    {
+        Region: "Abay",
+        Amoxicillin: 0.095124,
+        Ceftriaxone: 0.085395,
+        Ciprofloxacin: 0.090829
+    },
+    {
+        Region: "Akmola",
+        Amoxicillin: 0.071794,
+        Ceftriaxone: 0.035800,
+        Ciprofloxacin: 0.126004
+    },
+    {
+        Region: "Aktobe",
+        Amoxicillin: 0.089371,
+        Ceftriaxone: 0.057199,
+        Ciprofloxacin: 0.103205
+    }
+];
