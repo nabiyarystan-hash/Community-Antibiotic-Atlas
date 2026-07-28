9c58da2cd097fce24e5ef1b5cd51868843b71b5a
nabiyarystan-hash <nabiy.arystan@nu.edu.kz>
2026-07-28 14:05:38 +0500

    Update tableData.js

--- tableData.js
+++ tableData.js
@@ -1,20 +1,115 @@
 const tableData = [
-    {
-        Region: "Abay",
-        Amoxicillin: 0.095124,
-        Ceftriaxone: 0.085395,
-        Ciprofloxacin: 0.090829
-    },
-    {
-        Region: "Akmola",
-        Amoxicillin: 0.071794,
-        Ceftriaxone: 0.035800,
-        Ciprofloxacin: 0.126004
-    },
-    {
-        Region: "Aktobe",
-        Amoxicillin: 0.089371,
-        Ceftriaxone: 0.057199,
-        Ciprofloxacin: 0.103205
-    }
+
+{
+Region:"Abay",
+Amoxicillin:0.095124,
+Ceftriaxone:0.085395,
+Ciprofloxacin:0.090829,
+Cefazolin:0.071514,
+Azithromycin:0.180842,
+Levofloxacin:0.029988,
+Gentamicin:0.000338,
+Doxycycline:0.042878,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.092448
+},
+
+{
+Region:"Akmola",
+Amoxicillin:0.071794,
+Ceftriaxone:0.035800,
+Ciprofloxacin:0.126004,
+Cefazolin:0.043113,
+Azithromycin:0.090570,
+Levofloxacin:0.019405,
+Gentamicin:0.000158,
+Doxycycline:0.045384,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.023896
+},
+
+{
+Region:"Aktobe",
+Amoxicillin:0.089371,
+Ceftriaxone:0.057199,
+Ciprofloxacin:0.103205,
+Cefazolin:0.045789,
+Azithromycin:0.092809,
+Levofloxacin:0.044215,
+Gentamicin:0.000338,
+Doxycycline:0.017231,
+Ampicillin:0.015463,
+"Amoxicillin/Clavulanate":0.063993
+},
+
+{
+Region:"Almaty city",
+Amoxicillin:0.017807,
+Ceftriaxone:0.043929,
+Ciprofloxacin:0.046011,
+Cefazolin:0.008781,
+Azithromycin:0.040750,
+Levofloxacin:0.009160,
+Gentamicin:0.000101,
+Doxycycline:0.009798,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.020253
+},
+
+{
+Region:"Almaty region",
+Amoxicillin:0.026071,
+Ceftriaxone:0.066485,
+Ciprofloxacin:0.061891,
+Cefazolin:0.030469,
+Azithromycin:0.086485,
+Levofloxacin:0.013935,
+Gentamicin:0.000277,
+Doxycycline:0.023711,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.031340
+},
+
+{
+Region:"Astana",
+Amoxicillin:0.046868,
+Ceftriaxone:0.072996,
+Ciprofloxacin:0.071517,
+Cefazolin:0.024158,
+Azithromycin:0.100228,
+Levofloxacin:0.017099,
+Gentamicin:0.000127,
+Doxycycline:0.035356,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.121165
+},
+
+{
+Region:"Atyrau",
+Amoxicillin:0.027185,
+Ceftriaxone:0.036772,
+Ciprofloxacin:0.016082,
+Cefazolin:0.048762,
+Azithromycin:0.059376,
+Levofloxacin:0.052787,
+Gentamicin:0.000116,
+Doxycycline:0.016634,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.033764
+},
+
+{
+Region:"East Kazakhstan",
+Amoxicillin:0.096287,
+Ceftriaxone:0.066947,
+Ciprofloxacin:0.083958,
+Cefazolin:0.013427,
+Azithromycin:0.134991,
+Levofloxacin:0.030049,
+Gentamicin:0.000112,
+Doxycycline:0.040721,
+Ampicillin:0.000000,
+"Amoxicillin/Clavulanate":0.067854
+}
+
 ];
